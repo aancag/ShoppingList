a5fc220ffa7dee79c3010ce22996699ce6254132
anca.antonie <anca.antonie@teamnet.ro>
2014-12-05 17:46:45 +0200

    update

--- w4_angularhr/scripts/controllers/MainController.js
+++ w4_angularhr/scripts/controllers/MainController.js
@@ -1,52 +1,96 @@
 /**
  * Created by Anca on 22.10.2014.
  */
-hrApp.controller('MainController', ['$scope', function($scope) {
+hrApp.controller('MainController', ['$scope', '$http', function($scope, $http) {
     $scope.title = 'Shopping List';
-    $scope.products = [
-        {
-            "name" : "castraveti",
-            "quantity" : "2kg",
-            "edit": false,
-            "bought" : false
-        },
-        {
-            "name" : "ciocolata",
-            "quantity" : "fara numar",
-            "edit" : false,
-            "bought" : false
-        }
-    ];
+    $scope.products = [];
+    $http({url:'http://localhost:34029/api/Values',
+            method:'GET'}
+    )
+        .success(function (data) {//if request succeed
+            $scope.products = data;
+            console.log(data);
+            console.log("get ok");
+        }).error(function(data){
+            console.log("get error");
+        });
 
     $scope.descriptionShow = false;
     $scope.toggleDescriptionShow = function() {
         $scope.descriptionShow = !$scope.descriptionShow;
     };
 
+    //add button
     $scope.create = function (product) {
+        product =  {
+            "Name" : product.Name,
+            "Quantity" : product.Quantity,
+            "Edit" : product.Edit,
+            "Bought" : product.Bought
+        };
         $scope.products.push(
-            {
-                "name" : product.name,
-                "quantity" : product.quantity,
-                "edit" : product.edit,
-                "bought" : product.bought
+            product
+        );
+
+        $http({url:'http://localhost:34029/api/Values/Post',
+            method:'POST',
+            dataType:"json",
+            data:{
+                Name : product.Name,
+                Quantity : product.Quantity,
+                Bought : product.Bought
+            }}
+        )
+            .success(function (data) {//if request succeed
+                console.log("post ok");
+            }).error(function(data){
+                console.log("post error");
             }
         );
     }
 
-    $scope.toggleEditShow = function(product) {
-        product.edit = !product.edit;
+    $scope.toggleEditShow = function(product, name, quantity) {
+        product.Edit = !product.Edit;
+        if(product.Edit == false){
+            product.Name = name;
+            product.Quantity = quantity;
+            $http({url:'http://localhost:34029/api/Values/' + product._id,
+                    method:'PUT', data:product}
+            )
+                .success(function (data) {
+                    console.log("put ok");
+                }).error(function(data){
+                    console.log("put error");
+                }
+            );
+
+
+        }
     };
 
     $scope.toggleBoughtShow = function(product) {
-        product.bought = !product.bought;
+        product.Bought = !product.Bought;
     };
 
     $scope.deleteBoughtProducts = function(){
           for(var i = 0; i < $scope.products.length; i++){
-            if($scope.products[i].bought == true){
-                $scope.products.splice(i, 1);
-                i--;
+            if($scope.products[i].Bought == true){
+
+                $http({url:'http://localhost:34029/api/Values/' + $scope.products[i]._id,
+                        method:'DELETE'}
+                )
+                    .success(function (data) {//if request succeed
+                        $http({url:'http://localhost:34029/api/Values',
+                                method:'GET'}
+                        )
+                            .success(function (data) {//if request succeed
+                                $scope.products = data;
+                            }).error(function(data){
+                                console.log("get error");
+                            });
+                    }).error(function(data){
+                        console.log("delete error");
+                    });
             }
         }
     }
